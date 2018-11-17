859f53d51538526b4e9e9a712cef23d4e138dc55
Jeff Moss <mosspilot@users.noreply.github.com>
2018-11-17 13:40:32 -0800

    Update URL parser
    The URL parser being used by mongoose.connect was a deprecated one,
    so we will ensure that we are using the new one by passing the
    useNewUrlParser flag in the connection options.

--- app.js
+++ app.js
@@ -59,8 +59,7 @@ passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
 // Set up the mongoose connection
-mongoose.connect('mongodb://localhost/auth-exercise');
-
+mongoose.connect('mongodb://localhost/auth-exercise', {'useNewUrlParser': true});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
